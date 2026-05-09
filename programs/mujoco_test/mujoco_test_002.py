8094edc1bbc1703c60ca39b26257f3e8fa21c5ad
Yuuki1645 <12107789+yuuki1645@users.noreply.github.com>
2026-05-09 18:01:20 +0900

    update

--- programs/mujoco_test/mujoco_test_002.py
+++ programs/mujoco_test/mujoco_test_002.py
@@ -3,12 +3,30 @@
 import mujoco
 import mujoco.viewer
 import time
+import math
 
 model = mujoco.MjModel.from_xml_path("xmls/main.xml")
 data = mujoco.MjData(model)
 
+# actuator名からid取得
+left_knee_id = mujoco.mj_name2id(
+    model,
+    mujoco.mjtObj.mjOBJ_ACTUATOR,
+    "left_knee_motor",
+)
+
 with mujoco.viewer.launch_passive(model, data) as viewer:
-  while viewer.is_running():
-    mujoco.mj_step(model, data)
-    viewer.sync()
-    time.sleep(model.opt.timestep)
+    start = time.time()
+
+    while viewer.is_running():
+        t = time.time() - start
+
+        # 角度指定：sinでゆっくり膝を動かす
+        target_angle = 0.4 * math.sin(t)
+
+        # position actuator の目標角度を指定
+        data.ctrl[left_knee_id] = target_angle
+
+        mujoco.mj_step(model, data)
+        viewer.sync()
+        time.sleep(model.opt.timestep)

fec7bde6ca4ef868298c0ae7efe92f8cbf771a1d
Yuuki1645 <12107789+yuuki1645@users.noreply.github.com>
2026-05-14 19:26:15 +0900

    update

--- mujoco-sim/programs/mujoco_test_001.py
+++ mujoco-sim/programs/mujoco_test_001.py
@@ -9,19 +9,21 @@ model = mujoco.MjModel.from_xml_path("../mujoco_sim_assets/xmls/005_leg_1joint/m
 data = mujoco.MjData(model)
 
 model.vis.map.alpha = 0.5
-model.vis.scale.jointlength = 0.6
+model.vis.scale.jointlength = 0.3
 model.vis.scale.jointwidth = 0.1
 model.vis.scale.framelength = 0.8
 model.vis.scale.framewidth = 0.04
+model.vis.scale.com = 0.1
+model.vis.rgba.com = (1, 0, 0, 1)
 model.vis.headlight.ambient = 0.5
 model.vis.headlight.diffuse = 1
 model.vis.headlight.specular = 1
 
 with mujoco.viewer.launch_passive(model, data) as viewer:
-
   viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_JOINT] = 1
   viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_TRANSPARENT] = 1
-  # viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_CENTEROFMASS] = 1
+  viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_COM] = 1
+  viewer.opt.label = mujoco.mjtLabel.mjLABEL_GEOM
   viewer.opt.frame = mujoco.mjtFrame.mjFRAME_BODY
 
   while viewer.is_running():

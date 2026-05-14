2b5af7f309f5ada7acb66bfdb7aabddf80bf50a9
Yuuki1645 <12107789+yuuki1645@users.noreply.github.com>
2026-05-14 13:00:39 +0900

    update

--- mujoco-sim/programs/mujoco_test_001.py
+++ mujoco-sim/programs/mujoco_test_001.py
@@ -9,6 +9,11 @@ model = mujoco.MjModel.from_xml_path("../mujoco_sim_assets/xmls/005_leg_1joint/m
 data = mujoco.MjData(model)
 
 with mujoco.viewer.launch_passive(model, data) as viewer:
+  viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_JOINT] = 1
+  viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_TRANSPARENT] = 1
+  viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_CENTEROFMASS] = 1
+  viewer.opt.frame = mujoco.mjtFrame.mjFRAME_BODY
+
   while viewer.is_running():
     mujoco.mj_step(model, data)
     viewer.sync()

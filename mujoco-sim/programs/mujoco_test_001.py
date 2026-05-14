0fbc313661b1a9acdefb90434d2ab05da1932962
Yuuki1645 <12107789+yuuki1645@users.noreply.github.com>
2026-05-14 19:34:19 +0900

    update

--- mujoco-sim/programs/mujoco_test_001.py
+++ mujoco-sim/programs/mujoco_test_001.py
@@ -3,28 +3,15 @@
 import mujoco
 import mujoco.viewer
 import time
-
+from mujoco_sim_common.viewer_visual_presets import apply_model_visual_preset, apply_passive_viewer_options
 
 model = mujoco.MjModel.from_xml_path("../mujoco_sim_assets/xmls/005_leg_1joint/main.xml")
 data = mujoco.MjData(model)
 
-model.vis.map.alpha = 0.5
-model.vis.scale.jointlength = 0.3
-model.vis.scale.jointwidth = 0.1
-model.vis.scale.framelength = 0.8
-model.vis.scale.framewidth = 0.04
-model.vis.scale.com = 0.1
-model.vis.rgba.com = (1, 0, 0, 1)
-model.vis.headlight.ambient = 0.5
-model.vis.headlight.diffuse = 1
-model.vis.headlight.specular = 1
+apply_model_visual_preset(model)
 
 with mujoco.viewer.launch_passive(model, data) as viewer:
-  viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_JOINT] = 1
-  viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_TRANSPARENT] = 1
-  viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_COM] = 1
-  viewer.opt.label = mujoco.mjtLabel.mjLABEL_GEOM
-  viewer.opt.frame = mujoco.mjtFrame.mjFRAME_BODY
+  apply_passive_viewer_options(viewer)
 
   while viewer.is_running():
     mujoco.mj_step(model, data)
